03229ab93f5eeaba08f2d5c123905661ffd5deed
Sasaki Yuto <reggaepunch@gmail.com>
2019-01-01 23:39:31 +0700

    Bulk update

--- cli.js
+++ cli.js
@@ -1,74 +1,63 @@
 #!/usr/bin/env node
 "use strict";
 
-const path = require('path');
+const fs = require('fs');
+const readline = require('readline');
 const sgf = require('staged-git-files');
 const symbols = require("log-symbols");
 
-sgf('ACM', (err, files) => {
+const black   = '\u001b[30m';
+const red     = '\u001b[31m';
+const green   = '\u001b[32m';
+const yellow  = '\u001b[33m';
+const blue    = '\u001b[34m';
+const magenta = '\u001b[35m';
+const cyan    = '\u001b[36m';
+const white   = '\u001b[37m';
+
+const reset   = '\u001b[0m';
+
+/*
+ * 
+ * 
+ ---------------------------------------- */
+sgf('ACMR', (err, files) => {
   if (err) {
     console.error(err);
   }
 
   files.forEach((file) => {
-    if (checkWhiteSpace(file.filename)) {
-      console.error('  ' + symbols['error'], file.filename + ' ["white space" is invalid file name. Remove it.]');
-      process.exit(1);
-    }
+    let line = 0;
 
-    if (checkFullWidthLetter(file.filename)) {
-      console.error('  ' + symbols['error'], file.filename + ' ["full width letter" is invalid file name. Change to half width.]');
-      process.exit(1);
-    }
+    const rl = readline.createInterface({
+      input: fs.createReadStream(file.filename),
+      output: null,
+      terminal: false
+    });
 
-    const pathObject = path.parse(file.filename);
-    const name = pathObject['name'];
-    const extension = pathObject['ext'];
+    rl.on('line', function (data) {
+      line++;
 
-    if (checkSymbol(name)) {
-      console.error('  ' + symbols['error'], name + ' is invalid');
-      process.exit(1);
-    }
+      if (data.search('^<<<<<<< HEAD$') > -1 || data.search('^=======$') > -1 || data.search('^>>>>>>>') > -1) {
+        putError(line, file);
+      }
 
-    if (matchUppercase(file.filename)) {
-      console.error('  ' + symbols['error'], file.filename + '. File name should be lowercase.');
-      process.exit(1);
-    }
+    });
 
-    if (matchUppercase(extension)) {
-      console.error('  ' + symbols['error'], extension + '. File extension should be lowercase.');
+    rl.on('close', function() {
       process.exit(1);
-    }
-
-    if (checkJpeg(extension)) {
-      console.error('  ' + symbols['error'], extension + '. Should be ".jpg".');
-      process.exit(1);
-    }
+    });
   });
 });
 
-const checkWhiteSpace = function (fileName) {
-  return fileName.match(/\s/);
+/*
+ * 
+ * 
+ ---------------------------------------- */
+const putError = function (line, file) {
+  console.error('\n ' + symbols['error'], magenta + `${file.filename} Line: ${line} is conflicted. Please fix conflict before create commit.` + reset + '\n');
+
+  process.exit(1);
 };
 
-const checkFullWidthLetter = function (fileName) {
-  return fileName.match(/[^\x01-\x7E]/);
-};
-
-const checkSymbol = function (fileName) {
-  return fileName.match(/[!"#$%&'()\*,\/:;<=>?\[\\\]^`{|}~]/);
-};
-
-const matchUppercase = function (extension) {
-  return extension.match(/[A-Z]/);
-};
-
-const checkJpeg = function (extension) {
-  return extension === '.jpeg';
-};
-
-module.exports.checkWhiteSpace = checkWhiteSpace;
-module.exports.checkFullWidthLetter = checkFullWidthLetter;
-module.exports.checkSymbol = checkSymbol;
-module.exports.matchUppercase = matchUppercase;
-module.exports.checkJpeg = checkJpeg;
+module.exports.putError = putError;
